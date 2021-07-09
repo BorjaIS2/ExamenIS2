2ba7d05eb619d9a167e02b5da14016301cbfa4c3
BorjaIS2 <73837279+BorjaIS2@users.noreply.github.com>
2021-07-09 14:19:34 +0100

    Add files via upload

--- src/aplication/Main.java
+++ src/aplication/Main.java
@@ -5,6 +5,13 @@
  */
 package aplication;
 
+import controller.Controller;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+import model.Statue;
+import model.Thematic;
+import view.PatrimonialView;
+
 /**
  *
  * @author Borja
@@ -12,7 +19,15 @@ package aplication;
 public class Main {
     
     public static void main(String[] args) {
+        try {
+        UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (UnsupportedLookAndFeelException | ClassNotFoundException | InstantiationException | IllegalAccessException e) {
+        }
         
+  Statue patrimonialElement = new Statue("David", "Miguel Angel",1501,"manual",["Marmol","Tallado"],"buen estado","Florencia,Venecia",[Thematic.thematic.art,Thematic.thematic.architecture]);
+  PatrimonialView view = new PatrimonialView(patrimonialElement);
+  Controller controller = new Controller(patrimonialElement, view);
+  controller.execute();
     }
     
 }
